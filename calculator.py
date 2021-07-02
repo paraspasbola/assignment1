bd512f468268a84640795174ed6d74d61051a14a
paraspasbola <69096712+paraspasbola@users.noreply.github.com>
2021-07-02 16:28:52 +0530

    day1 assignment

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#calculator
+
+a = int(input ("enter first number :"))
+b = int(input ("enter second number :"))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a//b #integer division
+div2=a/b #floating point division
+print("\n sum :",sum)
+print(" sub :",sub)
+print(" mul :",mul)
+print(" div :",div)
+print(" div2 :",div2)
+
+
+
+print("thank you....") 
